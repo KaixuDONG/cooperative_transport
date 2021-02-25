da3062c59160e15910d93867511a6320283d0c36
KaixuDONG <kaixudong2-c@my.cityu.edu.hk>
2021-02-25 18:00:43 +0800

    the initial PID controller for the position loop

--- test_only.py
+++ test_only.py
@@ -5,7 +5,7 @@ print(controllers.SaturationFunction(12.6, 15, 100))
 
 kk = controllers.PID_ControllerThreeAixs(1000, 1, 1, 1, 1,
                                          1, 1, 1, 1,
-                                         1, 1, 1, 1)
+                                         1, 1, 1, 10)
 
 print(kk.pid_x.kp)
 print(kk.pid_x.integration_saturation_flg)
